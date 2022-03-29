da395f0a0d3c95ef86b360555237912dd09f757f
Breno Silva Rezende <brenorezende890@gmail.com>
2022-03-29 20:21:04 -0300

    alteração

--- pratica.java
+++ pratica.java
@@ -1,6 +1,8 @@
 public class pratica {
     public static void main(String[] args) {
-int num1 = 3, num2 = 5, soma;
-soma = num1 + num2;
+        int num1 = 3, num2 = 5,soma;
+        soma = num1 + num2;
         System.out.println(soma);
+        
+    }
 }
